389d06fa5573022af5da58543b26531eaa3cbba3
rexysiwabessy <rexysiwabessy@gmail.com>
2021-04-24 00:06:55 +0700

    rexy

--- app/Model/ActivityTracking.php
+++ app/Model/ActivityTracking.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActivityTracking extends CompModel
 {
-    
+    protected static function boot() {
+        static::creating(function($model)
+        {
+            $lastCodeId = ActivityTracking::max('id');
+            $code = str_repeat("0", 4 - strlen($lastCodeId)).($lastCodeId + 1);
+            $model->activity_trackings_code = 'ACT-'.date('Ymd').$code;
+        });
+
+        parent::boot();
+    }
 }
