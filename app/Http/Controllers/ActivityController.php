9272ac8dfa14968b899e0e37e168b1aa408184cd
rexysiwabessy <rexysiwabessy@gmail.com>
2021-04-27 01:03:21 +0700

    rexy

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -8,6 +8,7 @@ use App\Constants\ErrorMessage as EM;
 use App\User;
 use App\Helper;
 use App\Polyline;
+use App\Model\UserClient;
 
 class ActivityController extends Controller
 {
@@ -23,26 +24,30 @@ class ActivityController extends Controller
 
     public function getList(Request $request) {
         $users = User::getUser( $request->bearerToken());
+        $userClientName = UserClient::getName($users->email_client);
         $activity_tracking = ActivityTracking::where('activity_trackings_code', $request->activity_trackings_code)->first();
-        $activity_tracking->athlete_name = $users->name_client;
+        $activity_tracking->athlete_name = $userClientName;
         $activity_tracking_decode = Polyline::decode($activity_tracking->polyline);
         $activity_tracking_pair = Polyline::pair($activity_tracking_decode);
         $activity_tracking->tracking = $activity_tracking_pair;
         $activity_tracking->pace_km = json_decode($activity_tracking->pace_km, true);
+        $activity_tracking->date = date('Y-m-d H:i:s', strtotime($activity_tracking->created_at));
 
         return Helper::responseData($activity_tracking);
     }
 
     public function getListMember(Request $request) {
         $users = User::getUser( $request->bearerToken());
+        $userClientName = UserClient::getName($users->email_client);
         $activity_tracking = ActivityTracking::where('athlete_id', $request->athlete_id)->orderBy('id', 'DESC')->get();
         
         foreach($activity_tracking as $key => $val) {
-            $val->athlete_name = $users->name_client;;
+            $val->athlete_name = $userClientName;;
             $activity_tracking_decode = Polyline::decode($val->polyline);
             $activity_tracking_pair = Polyline::pair($activity_tracking_decode);
             $val->tracking = $activity_tracking_pair;
             $val->pace_km = json_decode($val->pace_km, true);
+            $val->date = date('Y-m-d H:i:s', strtotime($val->created_at));
         }
 
         return Helper::responseData($activity_tracking);
@@ -84,6 +89,7 @@ class ActivityController extends Controller
                 $activity_tracking_pair = Polyline::pair($activity_tracking_decode);
                 $val->tracking = $activity_tracking_pair;
                 $val->pace_km = json_decode($val->pace_km, true);
+                $val->created_at = date('Y-m-d H:i:s', strtotime($val->created_at));
             }
 
             return Helper::responseData($activity_tracking);
--- app/Model/UserClient.php
+++ app/Model/UserClient.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class UserClient extends CompModel
 {
     
+    static function getName($param) {
+        $name = UserClient::where('email_client', $param)->select('name_client')->first();
+
+        return $name->name_client;
+    }
 }
