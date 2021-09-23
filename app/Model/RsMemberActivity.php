0e6470066e8e7fdba2cf7ff2b89389f4a1c6f79d
rexy siwabessy <rexysiwabessy@gmail.com>
2021-09-23 16:51:50 +0700

    rexy update bugs

--- app/Model/RsMemberActivity.php
+++ app/Model/RsMemberActivity.php
@@ -3,6 +3,7 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class RsMemberActivity extends Model
 {
@@ -10,8 +11,9 @@ class RsMemberActivity extends Model
     protected $table = 'rs_member_activity';
     
     static function getDistance($param) {
+        $joindate = new Carbon($param->created_at);
         $result = RsMemberActivity::where('athlete_id', $param->athlete_id)
-                  ->whereDate('start_date', '>=', $param->created_at)
+                  ->whereDate('start_date', '>=', $joindate->addHours(12))
                   ->whereDate('start_date', '<=', $param->to_date)
                   ->sum('distance');
 
